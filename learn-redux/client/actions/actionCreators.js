c4a7e200e8c46e7e23002a2bfdf6f02c69211e39
Andrew Nguyen <andy.andino93@gmail.com>
2017-11-23 18:50:39 -0800

    add action creator

--- learn-redux/client/actions/actionCreators.js
+++ learn-redux/client/actions/actionCreators.js
@@ -1,5 +1,25 @@
 // increment
+export const increment = (index) => {
+  return {
+    type: 'INCREMENT_LIKES',
+    index,
+  }
+};
 
 // add comment
-
+export const addComment = (postId, author, comment) => {
+  return {
+    type: 'ADD_COMMENT',
+    postId,
+    author,
+    comment,
+  }
+};
 // remove comment
+
+export const removeComment = (postId, index) => {
+  return {
+    type: 'REMOVE_COMMENT',
+    i,
+  }
+};
