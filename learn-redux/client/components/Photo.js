af8faf152ad3c64cf08d17aa09e2d36cfc79fdd1
Andrew Nguyen <andy.andino93@gmail.com>
2017-11-24 14:25:21 -0800

    add like reducer

--- learn-redux/client/components/Photo.js
+++ learn-redux/client/components/Photo.js
@@ -11,7 +11,6 @@ const Photo = React.createClass({
           <Link to={`/view/${post.code}`}>
             <img src={post.display_src} alt={post.caption} className="grid-photo" />
           </Link>
-
           <CSSTransitionGroup
             transitionName="like"
             transitionEnterTimeout={500}
@@ -19,9 +18,19 @@ const Photo = React.createClass({
           >
             <span key={post.likes} className="likes-heart">{post.likes}</span>
           </CSSTransitionGroup>
-
         </div>
-
+        <figcaption>
+          <p>{post.caption}</p>
+          <div className="control-buttons">
+            <button onClick={() => this.props.increment(i)} className="likes">&hearts; {post.likes}</button>
+            <Link className="button" to={`/view/${post.code}`}>
+              <span className="comment-count">
+                <span className="speech-bubble"></span>
+                {comments[post.code] ? comments[post.code].length : 0}
+              </span>
+            </Link>
+          </div>
+        </figcaption>
       </figure>
     )
   }
--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -1,8 +1,17 @@
 // reducer takes in two things. action and copy of current state
 
 const posts = (state = [], action) => {
-  console.log(state, action);
-  return state;
+  switch(action.type) {
+    case 'INCREMENT_LIKES':
+      const i = action.index;
+      return [
+        ...state.slice(0, i),
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1)
+      ]
+    default:
+    return state;
+  }
 };
 
 export default posts;
